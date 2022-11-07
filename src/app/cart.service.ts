fab610dfee7118e065f57410194ba586279a5256
AlinaElena3098 <38075203+AlElM398@users.noreply.github.com>
2022-11-07 15:40:36 +0100

    adding an array and methods to the cart service

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,9 +1,25 @@
+import { Product } from './products'; //import product interface
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class CartService {
+  items: Product[] = []; //define an items property to store the array of the current products in the cart
 
-  constructor() { }
+  //define methods to add items to the cart
+  addToCart(product: Product) {
+    this.items.push(product);
+  }
+
+  //return cart items
+  getItems() {
+    return this.items;
+  }
+
+  //clear the cart items
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
 }
